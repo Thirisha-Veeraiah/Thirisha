f022d0669ee568b78d0924c3a1bb8d639d8e3dab
Thirisha-Veeraiah <151051811+Thirisha-Veeraiah@users.noreply.github.com>
2023-11-16 22:15:45 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,20 @@
+class studentinformation:
+  def __init__(self):
+    self.name=""
+    self.rollno=""
+    self.dob=""
+  def display(self):
+    print("name=",self.name)
+    print("rollno=",self.rollno)
+    print("dob=",self.dob)
+
+s1=studentinformation()   
+
+s1.name="naveen"
+s1.rollno="23"
+s1.dob="11.4.2003"
+
+s1.display()
+
+
+    
